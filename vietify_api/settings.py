0d54fe1392b9e7211ca255937f1f4aaff8352683
quangviet910@gmail.com <quangviet910@gmail.com>
2020-10-04 11:29:36 +0700

    AWS VARS

--- vietify_api/settings.py
+++ vietify_api/settings.py
@@ -168,7 +168,7 @@ if MODE == 'dev':
 elif MODE == 'prod':
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('MUSIC_BUCKET_NAME')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     PUBLIC_MEDIA_LOCATION = 'media'
